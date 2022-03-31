5d0cdc853a1d26962e002001681d5ba42e261377
GamGyu <barry0105@Naver.com>
2022-03-31 10:04:25 +0900

    Create format_.py

---
+++ format_.py
@@ -0,0 +1,14 @@
+s = "name: {}, number: {}, soccer:{}"
+
+format_s = s.format("SON", 7, True)
+print(format_s)
+
+format_a = "{}만원".format(5000)
+print(format_a)
+
+format_b = "파이썬 열공하여 연봉 {}만 원 만들기".format(5000)
+print(format_b)
+
+format_c = "{}과 {}학년 학생?: {}".format("정보통신",3,True)
+
+print(format_c)

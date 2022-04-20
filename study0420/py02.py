d0ded4ba964d09cd22060bfc36c07d737133e4c3
GamGyu <barry0105@Naver.com>
2022-04-20 11:09:55 +0900

    0420

---
+++ study0420/py02.py
@@ -0,0 +1,19 @@
+from dataclasses import replace
+
+
+print(331/17)
+
+x = 37
+print (x**2)
+
+x = 10
+print(x**x)
+
+print((123+456j).conjugate())
+
+fun = "funny day"
+print(fun)
+
+print(complex(9.87))
+
+print(fun.replace("day","life"))

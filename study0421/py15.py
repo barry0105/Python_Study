433e48f23ce499f1b090268666808b33e00e3edf
GamGyu <barry0105@Naver.com>
2022-04-21 11:41:23 +0900

    Update py15.py

--- study0421/py15.py
+++ study0421/py15.py
@@ -1,7 +1,14 @@
 import imp
 from tkinter import *
 window = Tk()
-
+def process1():
+    C = float(e2.get())
+    F = (9/5) * C + 32
+    e1.insert(0, str(F))
+def process2():
+    F = float(e1.get())
+    C = (F-32)*5/9
+    e2.insert(0, str(C))
 l1 = Label(window, text = "화씨")
 l2 = Label(window, text = "섭씨")
 l1.grid(row = 0,column = 0)
@@ -12,8 +19,8 @@ e2 = Entry(window)
 e1.grid(row = 0,column = 1)
 e2.grid(row = 1,column = 1)
 
-b1 = Button(window, text = "화씨에서 섭씨로")
-b2 = Button(window, text = "섭씨에서 화씨로")
+b1 = Button(window, text = "화씨에서 섭씨로", command=process1)
+b2 = Button(window, text = "섭씨에서 화씨로", command=process2)
 b1.grid(row = 2,column = 0)
 b2.grid(row = 2,column = 1)
 

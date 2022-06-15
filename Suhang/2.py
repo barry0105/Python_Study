37a6f06559fce47e1caa38fa466274e2e1b86d59
GamGyu <barry0105@Naver.com>
2022-06-15 10:38:36 +0900

    0615

---
+++ Suhang/2.py
@@ -0,0 +1,8 @@
+x = input()
+if x > 10:
+    if x % 2 == 0:
+        print("10초과 짝수")
+    else:
+        print("10초과 홀수")
+else:
+    print("10이하")
---
+++ Suhang/3.py
@@ -0,0 +1,3 @@
+our_pots={'일지매':1,'이무기':2,'삼지창':3,'사오정':4,'오징어':5}
+for i in our_pots.keys():
+    print(i)
